f560c87323fd44c8a103625480feb313dd81146b
Joseph Heng <lengerfulluse@gmail.com>
2013-05-26 00:41:49 +0800

    GUI module for demonstration of jokes with B/S, C/S modular

---
+++ JokePenny.py
@@ -0,0 +1,13 @@
+import sys
+import whoosh
+import re
+from __future__ import print_function
+
+"""GUI module for all the jokes, and mainly contains two GUI interfaces:
+   independent applications.
+   web query appearence.
+"""
+
+class JokePenny:
+    def __init__(self):
+    	pass
