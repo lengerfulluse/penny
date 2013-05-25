8dd2f8887eadcee5d764b5b89bc81b3b4d58cb7b
Joseph Heng <lengerfulluse@gmail.com>
2013-05-26 00:38:00 +0800

    JokeRanker module initialiation

---
+++ JokeRanker.py
@@ -0,0 +1,16 @@
+import sys
+import re
+import os
+
+"""Training all the jokes collected by the JokeCrawler module, and ranking it according to their features
+   Penny's personal like and also some automatic feature discovery algorithm.
+   
+   @Author Lengerfulluse@gmail.com
+   @since 0.1
+
+"""
+
+class JokeRanker:
+    def __init__(self):
+    	pass
+
